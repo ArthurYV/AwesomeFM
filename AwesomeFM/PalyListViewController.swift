9d2e942faed66f97ca573d7834fd14221d2c7587
ArthurYV <yangjingbing007@gmail.com>
2015-08-12 13:08:05 +0800

    再次删除SnapKit相关代码

--- AwesomeFM/PalyListViewController.swift
+++ AwesomeFM/PalyListViewController.swift
@@ -7,10 +7,9 @@
 //
 
 import UIKit
-import SnapKit
 
 class PalyListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-//    var playListTableView:UITableView = UITableView()
+    
     @IBOutlet weak var playListTableView: UITableView!
     
     override func viewDidLoad() {
@@ -20,16 +19,6 @@ class PalyListViewController: UIViewController, UITableViewDelegate, UITableView
     
     func _initTableView() {
         
-//        self.view.addSubview(playListTableView)
-//        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.size.height
-//        let navigationBarHeight = self.navigationController?.navigationBar.frame.size.height
-//        playListTableView.snp_makeConstraints { (make) -> Void in
-//            //要把布局约束全部描述清楚才可以
-//            make.top.equalTo(self.view).offset(navigationBarHeight! + statusBarHeight)
-//            make.left.equalTo(self.view).offset(0)
-//            make.bottom.equalTo(self.view).offset(0)
-//            make.right.equalTo(self.view).offset(0)
-//        }
         
         playListTableView.delegate = self
         playListTableView.dataSource = self
@@ -41,6 +30,7 @@ class PalyListViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
         cell.textLabel?.text = "Hello"
         return cell
